c1ea3fdd671e7b6361e1476b55b613b1a8ff3d08
Alexey Aleev <aleevas@gmail.com>
2018-12-01 12:45:19 +0200

    Replaced all to using a class instead procedural code

--- src/WhatIsYourName.php
+++ src/WhatIsYourName.php
@@ -2,9 +2,30 @@
 
 namespace Acme;
 
-use Symfony\Component\Console\Command;
+use Symfony\Component\Console\{
+    Command\Command,
+    Input\InputInterface,
+    Input\InputArgument,
+    Output\OutputInterface,
+    Output\Output
+};
+
 
 class WhatIsYourName extends Command {
 
+    public function configure () {
 
+        $this->setName('WhatIsYourName')
+             ->setDescription('Offer a greeting to the given person')
+             ->addArgument('name', InputArgument::REQUIRED, 'Please enter your name');
+
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output) {
+
+        $message = 'Glad to see you, ' . $input->getArgument('name') . '!';
+        $output->writeln("<info>{$message}</info>");
+
+    }
+    
 }
